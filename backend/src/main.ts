5d83010392d7f58fb7ec2ae564f1670f1b22d903
Steph4477 <morelstephan4026@neuf.fr>
2024-05-28 12:36:08 +0200

    build: vercel config update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,27 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
-import * as cookieParser from 'cookie-parser';
-import * as express from 'express';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use(cookieParser());
-  app.use(express.json({ limit: '50mb' }));
-  app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
   app.enableCors({
-    //origin: 'http://192.168.1.113:8080', //network simplon
-    //origin: 'http://192.168.0.45:8080', //network perso
     origin: 'http://localhost:8080',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  
   await app.listen(process.env.PORT || 3000);
+  return app;
 }
 
-bootstrap();
+const app = bootstrap();
+
+module.exports = app;
+
+export default app;
