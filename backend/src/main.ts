04cd290cc66c59188f15b6ac71062842eced8767
Steph4477 <morelstephan4026@neuf.fr>
2024-05-29 09:31:34 +0200

    hopeless

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 
-dotenv.config();
+console.log('test');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:8080',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  await app.listen(process.env.PORT || 3000);
-  return app;
+  await app.listen(3000);
 }
-
-export default bootstrap();
-
+bootstrap();
 
 
 
