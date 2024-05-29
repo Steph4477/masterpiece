530b14d2da41394a7c7b8c607afad67e2926bc19
Steph4477 <morelstephan4026@neuf.fr>
2024-05-29 09:34:01 +0200

    enable typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 // import { MerchantModule } from './Merchant/merchant.module';
 // import { AuthModule } from './Auth/auth.module';
 // import { ProductModule } from './Product/product.module';
@@ -17,19 +17,19 @@ console.log({
 @Module({
   imports: [
     // MerchantModule,AuthModule,ProductModule,
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.DB_HOST,
-    //   port: parseInt(process.env.DB_PORT),
-    //   database: process.env.DB_DATABASE,
-    //   username: process.env.DB_USER,
-    //   password: process.env.DB_PASSWORD,
-    //   entities: [__dirname + '/**/*.entity.ts'],
-    //   synchronize: false,
-    //   autoLoadEntities: true,
-    //   logging: false,
-    //   ssl: true,
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: false,
+      autoLoadEntities: true,
+      logging: false,
+      ssl: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
