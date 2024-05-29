e6f7ba76250074ba2246b8bf6cb1bca593cd93ae
Steph4477 <morelstephan4026@neuf.fr>
2024-05-29 09:35:28 +0200

    fullapp

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { MerchantModule } from './Merchant/merchant.module';
-// import { AuthModule } from './Auth/auth.module';
-// import { ProductModule } from './Product/product.module';
+import { MerchantModule } from './Merchant/merchant.module';
+import { AuthModule } from './Auth/auth.module';
+import { ProductModule } from './Product/product.module';
 
 console.log({
   host: process.env.DB_HOST,
@@ -16,7 +16,9 @@ console.log({
 
 @Module({
   imports: [
-    // MerchantModule,AuthModule,ProductModule,
+    MerchantModule,
+    AuthModule,
+    ProductModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
