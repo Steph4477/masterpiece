2599880bf0c27f07cf024f2289c299c61013a30e
Steph4477 <morelstephan4026@neuf.fr>
2024-05-29 09:27:37 +0200

    light weight

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,14 +4,14 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:8080',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000);
   return app;
 }
 
